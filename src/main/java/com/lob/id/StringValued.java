6b249bedb308528e818f34ae2acbfd94f4bf0657
Donald Little <donald@lob.com>
2017-08-05 22:39:40 -0700

    chore(cleanup): delete old wrapper

--- src/main/java/com/lob/id/StringValued.java
+++
@@ -1,9 +0,0 @@
-package com.lob.id;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.lob.protocol.serializer.StringValuedSerializer;
-
-@JsonSerialize(using = StringValuedSerializer.class)
-public interface StringValued {
-    public String value();
-}
--- src/main/java/com/lob/protocol/request/HasLobParams.java
+++
@@ -1,7 +0,0 @@
-package com.lob.protocol.request;
-
-import java.util.Collection;
-
-public interface HasLobParams {
-    public Collection<LobParam> getLobParams();
-}
