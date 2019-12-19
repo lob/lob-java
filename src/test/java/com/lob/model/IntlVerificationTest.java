1b7079bec7c7cb1c5c04f75d1f483d12436cdf86
Matt <matt@lob.com>
2019-12-19 11:26:13 -0800

    chore(intl-ver): update intl ver test (#155)

--- src/test/java/com/lob/model/IntlVerificationTest.java
+++ src/test/java/com/lob/model/IntlVerificationTest.java
@@ -1,27 +1,27 @@
 package com.lob.model;
 
 import com.lob.BaseTest;
-import com.lob.exception.APIException;
+import com.lob.net.LobResponse;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class IntlVerificationTest extends BaseTest {
 
     @Test
     public void testInternationalVerification() throws Exception {
-        try {
-            new IntlVerification.RequestBuilder()
+        LobResponse<IntlVerification> response = new IntlVerification.RequestBuilder()
                     .setPrimaryLine("370 Water St")
                     .setCity("Summerside")
                     .setState("Prince Edward Island")
                     .setPostalCode("C1N 1C4")
                     .setCountry("CA")
                     .verify();
-        } catch (final APIException e) {
-            assertThat(e.getStatusCode(), is(403));
-        }
+
+        IntlVerification intlVerification = response.getResponseBody();
+
+        assertEquals(200, response.getResponseCode());
+        assertEquals("TEST KEYS DO NOT VERIFY ADDRESSES", intlVerification.getRecipient());
     }
 
 }
