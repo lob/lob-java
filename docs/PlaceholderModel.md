7fa1dbb03a8c96aa6de3f479b010a0ef724895e7
lobot <engineering@lob.com>
2022-08-25 17:26:46 +0000

    ci:

--- docs/PlaceholderModel.md
+++ docs/PlaceholderModel.md
@@ -9,6 +9,10 @@ Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **returnEnvelope** | [**ReturnEnvelope**](ReturnEnvelope.md) |  |  [optional]
 **addressDomestic** | [**AddressDomestic**](AddressDomestic.md) |  |  [optional]
+**letterDetailsWritable** | [**LetterDetailsWritable**](LetterDetailsWritable.md) |  |  [optional]
+**postcardDetailsWritable** | [**PostcardDetailsWritable**](PostcardDetailsWritable.md) |  |  [optional]
+**letterDetailsReturned** | [**LetterDetailsReturned**](LetterDetailsReturned.md) |  |  [optional]
+**postcardDetailsReturned** | [**PostcardDetailsReturned**](PostcardDetailsReturned.md) |  |  [optional]
 
 
 
--- src/main/java/com/lob/model/PlaceholderModel.java
+++ src/main/java/com/lob/model/PlaceholderModel.java
@@ -21,6 +21,10 @@ import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import com.lob.model.AddressDomestic;
+import com.lob.model.LetterDetailsReturned;
+import com.lob.model.LetterDetailsWritable;
+import com.lob.model.PostcardDetailsReturned;
+import com.lob.model.PostcardDetailsWritable;
 import com.lob.model.ReturnEnvelope;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -74,6 +78,90 @@ public class PlaceholderModel {
   
   
 
+  public static final String SERIALIZED_NAME_LETTER_DETAILS_WRITABLE = "letter_details_writable";
+
+  @SerializedName(SERIALIZED_NAME_LETTER_DETAILS_WRITABLE)
+  
+
+  private LetterDetailsWritable letterDetailsWritable;
+  /**
+  * Get letterDetailsWritable
+  * @return letterDetailsWritable
+  **/
+  
+  @javax.annotation.Nullable
+  
+  @ApiModelProperty(value = "")
+  
+  public LetterDetailsWritable getLetterDetailsWritable() {
+      return letterDetailsWritable;
+  }
+  
+  
+
+  public static final String SERIALIZED_NAME_POSTCARD_DETAILS_WRITABLE = "postcard_details_writable";
+
+  @SerializedName(SERIALIZED_NAME_POSTCARD_DETAILS_WRITABLE)
+  
+
+  private PostcardDetailsWritable postcardDetailsWritable;
+  /**
+  * Get postcardDetailsWritable
+  * @return postcardDetailsWritable
+  **/
+  
+  @javax.annotation.Nullable
+  
+  @ApiModelProperty(value = "")
+  
+  public PostcardDetailsWritable getPostcardDetailsWritable() {
+      return postcardDetailsWritable;
+  }
+  
+  
+
+  public static final String SERIALIZED_NAME_LETTER_DETAILS_RETURNED = "letter_details_returned";
+
+  @SerializedName(SERIALIZED_NAME_LETTER_DETAILS_RETURNED)
+  
+
+  private LetterDetailsReturned letterDetailsReturned;
+  /**
+  * Get letterDetailsReturned
+  * @return letterDetailsReturned
+  **/
+  
+  @javax.annotation.Nullable
+  
+  @ApiModelProperty(value = "")
+  
+  public LetterDetailsReturned getLetterDetailsReturned() {
+      return letterDetailsReturned;
+  }
+  
+  
+
+  public static final String SERIALIZED_NAME_POSTCARD_DETAILS_RETURNED = "postcard_details_returned";
+
+  @SerializedName(SERIALIZED_NAME_POSTCARD_DETAILS_RETURNED)
+  
+
+  private PostcardDetailsReturned postcardDetailsReturned;
+  /**
+  * Get postcardDetailsReturned
+  * @return postcardDetailsReturned
+  **/
+  
+  @javax.annotation.Nullable
+  
+  @ApiModelProperty(value = "")
+  
+  public PostcardDetailsReturned getPostcardDetailsReturned() {
+      return postcardDetailsReturned;
+  }
+  
+  
+
 
   /*
   public PlaceholderModel returnEnvelope(ReturnEnvelope returnEnvelope) {
@@ -105,6 +193,66 @@ public class PlaceholderModel {
 
 
 
+  /*
+  public PlaceholderModel letterDetailsWritable(LetterDetailsWritable letterDetailsWritable) {
+    
+    this.letterDetailsWritable = letterDetailsWritable;
+    return this;
+  }
+  */
+
+
+  public void setLetterDetailsWritable(LetterDetailsWritable letterDetailsWritable) {
+    this.letterDetailsWritable = letterDetailsWritable;
+  }
+
+
+
+  /*
+  public PlaceholderModel postcardDetailsWritable(PostcardDetailsWritable postcardDetailsWritable) {
+    
+    this.postcardDetailsWritable = postcardDetailsWritable;
+    return this;
+  }
+  */
+
+
+  public void setPostcardDetailsWritable(PostcardDetailsWritable postcardDetailsWritable) {
+    this.postcardDetailsWritable = postcardDetailsWritable;
+  }
+
+
+
+  /*
+  public PlaceholderModel letterDetailsReturned(LetterDetailsReturned letterDetailsReturned) {
+    
+    this.letterDetailsReturned = letterDetailsReturned;
+    return this;
+  }
+  */
+
+
+  public void setLetterDetailsReturned(LetterDetailsReturned letterDetailsReturned) {
+    this.letterDetailsReturned = letterDetailsReturned;
+  }
+
+
+
+  /*
+  public PlaceholderModel postcardDetailsReturned(PostcardDetailsReturned postcardDetailsReturned) {
+    
+    this.postcardDetailsReturned = postcardDetailsReturned;
+    return this;
+  }
+  */
+
+
+  public void setPostcardDetailsReturned(PostcardDetailsReturned postcardDetailsReturned) {
+    this.postcardDetailsReturned = postcardDetailsReturned;
+  }
+
+
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -115,12 +263,16 @@ public class PlaceholderModel {
     }
     PlaceholderModel placeholderModel = (PlaceholderModel) o;
     return Objects.equals(this.returnEnvelope, placeholderModel.returnEnvelope) &&
-        Objects.equals(this.addressDomestic, placeholderModel.addressDomestic);
+        Objects.equals(this.addressDomestic, placeholderModel.addressDomestic) &&
+        Objects.equals(this.letterDetailsWritable, placeholderModel.letterDetailsWritable) &&
+        Objects.equals(this.postcardDetailsWritable, placeholderModel.postcardDetailsWritable) &&
+        Objects.equals(this.letterDetailsReturned, placeholderModel.letterDetailsReturned) &&
+        Objects.equals(this.postcardDetailsReturned, placeholderModel.postcardDetailsReturned);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(returnEnvelope, addressDomestic);
+    return Objects.hash(returnEnvelope, addressDomestic, letterDetailsWritable, postcardDetailsWritable, letterDetailsReturned, postcardDetailsReturned);
   }
 
   @Override
@@ -129,6 +281,10 @@ public class PlaceholderModel {
     sb.append("{\n");
     sb.append("    returnEnvelope: ").append(toIndentedString(returnEnvelope)).append("\n");
     sb.append("    addressDomestic: ").append(toIndentedString(addressDomestic)).append("\n");
+    sb.append("    letterDetailsWritable: ").append(toIndentedString(letterDetailsWritable)).append("\n");
+    sb.append("    postcardDetailsWritable: ").append(toIndentedString(postcardDetailsWritable)).append("\n");
+    sb.append("    letterDetailsReturned: ").append(toIndentedString(letterDetailsReturned)).append("\n");
+    sb.append("    postcardDetailsReturned: ").append(toIndentedString(postcardDetailsReturned)).append("\n");
     sb.append("}");
     return sb.toString();
   }
